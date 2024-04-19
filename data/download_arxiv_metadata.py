62ef7ea19b6e6bade2f52b374c911f1ab5522612
kkin1995 <7694983+kkin1995@users.noreply.github.com>
2024-04-19 15:13:25 +0530

    Created a package structure. Installed arxiv-public-data for downloading data. Setup Ruff as dev dependency for linting.

---
+++ data/download_arxiv_metadata.py
@@ -0,0 +1,11 @@
+from arxiv_public_data.oai_metadata import all_of_arxiv
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+DATA_DIR = os.environ.get("DATA_DIR")
+
+if not os.path.exists(DATA_DIR):
+    os.makedirs(DATA_DIR)
+
+all_of_arxiv(outfile=DATA_DIR)
